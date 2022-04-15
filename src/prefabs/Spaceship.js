54ad5bb7b8455c49c74cdeb9328e14dc58b55e75
Ryan Hueckel <77073352+jrhuecke@users.noreply.github.com>
2022-04-15 12:51:16 -0700

    random space movement maybe haha

--- src/prefabs/Spaceship.js
+++ src/prefabs/Spaceship.js
@@ -4,18 +4,31 @@ class Spaceship extends Phaser.GameObjects.Sprite {
         scene.add.existing(this);
         this.points = pointValue;
         this.moveSpeed = game.settings.spaceshipSpeed;
+        this.movingLeft = math.floor(math.random() * 2);
     }
 
     update() {
-        //move spaceship left
-        this.x -= this.moveSpeed;
-        //wrap around from left to right
-        if (this.x <= 0 - this.width) {
-            this.x = game.config.width;
+        //move spaceship left or right
+        if (this.movingLeft) {
+            this.x -= this.moveSpeed;
+        } else {
+            this.x += this.moveSpeed;
+        }   
+        //wrap around from left to right or vice versa
+        if (this.x <= 0 - this.width && this.movingLeft) {
+            this.reset();
+        }
+        if (this.x >= this.width + game.config.width && !this.movingLeft) {
+            this.reset();
         }
     }
 
     reset() {
-        this.x = game.config.width;
+        this.movingLeft = math.floor(math.random() * 2);
+        if (this.movingLeft) {
+            this.x = game.config.width;
+        } else {
+            this.x = 0
+        }
     }
 }
